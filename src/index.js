c8a0ad695468c4010549bda1a442129b6187dd19
samsvk <samsvk00@gmail.com>
2022-09-02 10:04:50 +0100

    fixed alignment issues

--- src/index.js
+++ src/index.js
@@ -20,43 +20,34 @@ function App() {
   return (
     <>
       <Grid />
-      <div className="max-w-[1200px] w-full mx-auto px-6 flex items-center justify-center flex-col">
-        <h1 className="text-6xl font-semibold tracking-tight z-[-10] mb-0 text-center text-spotify-green">
-          Enter Your Email
-        </h1>
-        <p className="text-[18px] text-center leading-7 font-normal tracking-tight text-neutral-700/60 my-8 max-w-[660px] w-full">
-          For the last time ever enter your email - gain access
-          to a healthier digital footprint, online-ecosystem and
-          protect your inbox.
-        </p>
-        {returnedValue}
-        <form
-          onSubmit={(e) => handleSubmit(e)}
-          className=" max-w-[440px] mx-auto w-full bg-gray-200/50 flex focus:outline focus:outline-offset-0 hover:outline hover:outline-offset-0 outline-gray-200 outline-2 hover:cursor-pointer min-h-[35px] rounded-full px-1 py-1 "
-        >
-          <p className="flex-1 px-4 pt-1 pb-1.5 flex items-center bg-transparent min-h-[35px] focus:outline-none focus:outline-offset-0 hover:outline-none hover:outline-offset-0 placeholder:text-base placeholder:text-[14px] placeholder:font-medium placeholder:tracking-tight placeholder:text-left placeholder:text-black/80 text-base text-[14px] font-medium tracking-tight text-left text-black/80 ">
-            Login with Spotify
-          </p>
-          <button
-            className="px-6 py-2 bg-black rounded-full text-[14px] leading-6 font-normal text-white hover:cursor-pointer hover:bg-black/80 duration-200"
-            type="submit"
-          >
-            Create Lobby
-          </button>
-        </form>
-        <div>
-          <p className="text-center mx-auto text-[14px] flex-1 mt-12 leading-5 font-normal text-neutral-700/80 my-10 tracking-tight flex gap-1 items-center">
-            <span className="underline">
-              Don't understand how it works?
+      <div className="mt-[20vh] max-w-[1150px] w-full mx-auto flex items-center">
+        <div className="flex flex-col flex-1">
+          <h1 className="text-7xl font-medium tracking-tighter z-[-10] mb-0 text-left text-spotify-text leading-[1.3]">
+            Create your new
+            <br /> playlist in{" "}
+            <span className="text-spotify-green">
+              One Click.
             </span>
-            <span className="line-through">$25</span>
-            now only{" "}
-            <span className="font-medium text-black/80">
-              $15
-            </span>
-            for unlimited masks.
+          </h1>
+          <p className="w-full my-8 text-2xl font-normal leading-7 tracking-tight text-left text-neutral-700/60">
+            Share your music taste with friends, and build a
+            playlist that suits all.
           </p>
+          <div>
+            <p className="text-center mx-auto text-[14px] flex-1 mt-12 leading-5 font-normal text-neutral-700/80 my-10 tracking-tight flex gap-1 items-center">
+              <span className="underline">
+                Don't understand how it works?
+              </span>
+              <span className="line-through">$25</span>
+              now only{" "}
+              <span className="font-medium text-black/80">
+                $15
+              </span>
+              for unlimited masks.
+            </p>
+          </div>
         </div>
+        <div className="flex flex-col flex-1">123</div>
       </div>
     </>
   );
