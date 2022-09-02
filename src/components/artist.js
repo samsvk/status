c884484be0b0b23a2c6bb2febe844de22113e496
samsvk <samsvk00@gmail.com>
2022-09-02 19:06:46 +0100

    fixed

--- src/components/artist.js
+++ src/components/artist.js
@@ -8,8 +8,8 @@ export default function Artist() {
   useEffect(() => {
     fetchRandomArtist(setRandomArtist);
   }, []);
+
   if (!randomArtist) return null;
-  // console.log(randomArtist);
   return (
     <div className="flex flex-col pl-8 ml-8">
       <div className="relative h-[440px] w-[350px] rounded-lg block after:absolute after:content-[''] after:h-full after:w-full after:bg-black/60 after:rounded-lg after:top-0 before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-bg before:bottom-2 before:left-2 before:border-2 before:border-spotify-text ">
@@ -17,7 +17,7 @@ export default function Artist() {
         <div className="absolute z-10 flex flex-col items-center justify-center w-full h-full m-auto top-[50%] translate-y-[-50%] translate-x-1/2">
           <div
             className="mr-7 block rounded-lg max-w-[154px] relative z-50 
-          before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-bg before:top-[-0.5rem] before:right-[-0.5rem] before:border-2 before:border-spotify-text before:-z-20 shadow-lg"
+          before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-bg before:top-[-0.25rem] before:right-[-0.25rem] before:border-2 before:border-spotify-text before:-z-20 shadow-lg"
           >
             <div className="block overflow-hidden border-2 rounded-lg border-spotify-text">
               <div className="h-[144px] w-[154px] overflow-hidden relative">
@@ -31,9 +31,9 @@ export default function Artist() {
           </div>
           <div
             className="block rounded-lg max-w-[154px] min-w-[154px] w-full relative z-50 
-          before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-green before:top-2 before:left-[-0.5rem] before:border-2 before:border-spotify-text before:-z-20
+          before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-green before:top-1 before:left-[-0.25rem] before:border-2 before:border-spotify-text before:-z-20
           shadow-lg
-          ml-12 mt-2.5
+          ml-12 mt-1.5
           "
           >
             <div className="block overflow-hidden border-2 rounded-lg border-spotify-text">
@@ -49,8 +49,8 @@ export default function Artist() {
           </div>
           <div
             className="block rounded-lg max-w-[154px] min-w-[154px]  w-full relative z-50 
-          before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-offset before:top-2 before:left-2 before:border-2 before:border-spotify-text before:-z-20
-          shadow-lg mr-12  mt-4
+          before:rounded-lg before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-offset before:top-1 before:left-1 before:border-2 before:border-spotify-text before:-z-20
+          shadow-lg mr-12  mt-2
           "
           >
             <div className="block overflow-hidden border-2 rounded-lg border-spotify-text">
