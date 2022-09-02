f9d404ad7a451167441569efb149d6ed55ece2f9
samsvk <samsvk00@gmail.com>
2022-09-02 09:46:18 +0100

    colors & display

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        colors: {
-          "0%": { color: "rgb(59 130 246)" },
-          "15%": { color: "rgb(234 179 8)" },
-          "30%": { color: "rgb(139 92 246)" },
-          "45%": { color: "rgb(99 102 241)" },
-          "60%": { color: "rgb(253 186 116)" },
-          "75%": { color: "rgb(168 85 247)" },
-          "90%": { color: "rgb(236 72 153)" },
+      colors: {
+        spotify: {
+          green: "#52784f",
+          bg: "#fdf7f2",
+          text: "#040403",
         },
-      },
-      animation: {
-        colors: "colors 20s linear infinite",
       },
     },
     plugins: [],
