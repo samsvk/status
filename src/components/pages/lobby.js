7bf7fd927a15b6cc2a45c603fbd2f3472b5b9f19
samsvk <samsvk00@gmail.com>
2022-09-10 11:05:54 +0100

    working ready up

--- src/components/pages/lobby.js
+++ src/components/pages/lobby.js
@@ -42,9 +42,7 @@ export default function Lobby() {
     if (!selected.some((item) => item.id == name.id)) {
       setSelected((_) => [..._, name]);
     } else {
-      setSelected((_) =>
-        _.filter((item) => item.id !== name.id)
-      );
+      setSelected((_) => _.filter((item) => item.id !== name.id));
     }
   }
 
@@ -65,8 +63,8 @@ export default function Lobby() {
   }, [SOCKET, userData]);
 
   if (!userData) return null;
-
   console.log(lobbyData);
+  console.log(playlistTunes[0]);
 
   return (
     <>
@@ -75,14 +73,6 @@ export default function Lobby() {
         <span>{item.name}</span>
       ))}
       <div className="flex gap-10">
-        <button
-          onClick={() =>
-            getUserPlaylistTracks(selected, setPlaylistTunes)
-          }
-        >
-          {" "}
-          get playlists
-        </button>
         {/* <button
           onClick={() =>
             createUserPlaylist({ ...userData, playlistTunes })
@@ -107,24 +97,21 @@ export default function Lobby() {
       <div className="relative max-w-[1250px] w-full mx-auto my-12 grid grid-cols-10 p-10">
         <div className="fixed col-span-5 max-w-[600px] w-full h-full">
           <h1 className="text-7xl font-medium tracking-tighter z-[-10]text-left text-spotify-text leading-tight">
-            Welcome to Shuff.le,{" "}
+            Welcome to Shuff.le,
             <span className=" first-letter:uppercase text-spotify-green">
               {userData.name}!
             </span>
           </h1>
           <p className="w-full max-w-lg mt-10 text-2xl font-normal leading-relaxed tracking-tight text-spotify-text">
-            You have {userData.playlists.length} playlists
-            available for shufflin', invite your friends and
-            we'll work our magic!
+            You have {userData.playlists.length} playlists available for shufflin',
+            invite your friends and we'll work our magic!
           </p>
           <button className="mt-10 flex items-center gap-4 px-8 py-5 text-xl font-normal leading-relaxed tracking-tight rounded-lg bg-spotify-green relative text-spotify-bg after:absolute after:content-[''] after:w-full after:h-full after:bottom-2 after:right-2 after:-z-10 after:rounded-lg after:bg-spotify-bg after:border-2 after:border-spotify-text hover:shadow-lg duration-200">
             <IoPeopleSharp size={27} />
             Invite your friends
           </button>
           <p className="text-center mx-auto mt-10 text-[14px] flex-1 leading-5 font-normal text-neutral-700/80  tracking-tight flex gap-1 items-center">
-            <span className="underline">
-              Don't have their email?
-            </span>
+            <span className="underline">Don't have their email?</span>
             Invite via this:
             <span className="font-medium text-black/80 hover:cursor-pointer hover:underline">
               Link
@@ -132,8 +119,8 @@ export default function Lobby() {
           </p>
 
           <ul className="overflow-y -scroll">
-            {lobbyData?.tracks?.map((item) => (
-              <li key={item.href}>{item.name}</li>
+            {lobbyData?.tracks?.map((item, index) => (
+              <li key={index}>{item.name}</li>
             ))}
           </ul>
         </div>
@@ -141,7 +128,7 @@ export default function Lobby() {
           {lobbyData?.users?.map((user, index) => {
             if (user == null) return null;
             return (
-              <aside className="" key={user._id}>
+              <aside className="" key={index}>
                 <div className="relative mx-auto mb-5 min-w-[120px] max-w-[120px] min-h-[50px]">
                   <div className="absolute z-20 w-full h-full">
                     <span className="absolute right-0 block w-2 h-2 scale-50 bg-yellow-500 rounded-full top-1 rotate-12" />
@@ -177,51 +164,57 @@ export default function Lobby() {
 
                 <div className="w-full h-full">
                   <div className="flex flex-col items-center justify-center gap-8 mx-auto mt-8 text-spotify-text max-w-max">
-                    {user.userData?.playlists?.map(
-                      (playlist, index) => {
-                        const sel = selected.some(
-                          (item) => item.id === playlist.id
-                        );
+                    {user.userData?.playlists?.map((playlist, index) => {
+                      const sel = selected.some((item) => item.id === playlist.id);
 
-                        return (
-                          <div
-                            key={index}
-                            className="relative flex flex-col flex-1 w-full col-span-1"
-                          >
-                            <div className="flex flex-row">
-                              <div
-                                onClick={() =>
-                                  addSelected({
-                                    name: playlist.name,
-                                    id: playlist.id,
-                                    length: playlist.length,
-                                  })
-                                }
-                                className={`
-                          relative w-14 h-14 before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-bg before:bottom-1 before:right-1 before:border-2 ${
-                            sel
-                              ? "before:border-spotify-text before:bg-spotify-green"
-                              : "before:border-spotify-text"
-                          }`}
-                              >
-                                <img
-                                  src={playlist.image}
-                                  className="relative block object-cover w-14 h-14 min-h-14 min-w-14 drop-shadow-lg"
-                                />
-                              </div>
-                              <div className="inline-flex flex-col items-start justify-end ml-2">
-                                <h3 className="block text-left text-[14px] font-normal tracking-tight first-letter:uppercase text-ellipsis whitespace-nowrap overflow-hidden">
-                                  {playlist.name}
-                                </h3>
-                                <p className="relative flex flex-wrap z-10 font-medium text-[10px] uppercase text-neutral-700/80  whitespace-nowrap">
-                                  Songs: #{playlist.length}
-                                </p>
-                              </div>
+                      return (
+                        <div
+                          key={index}
+                          className="relative flex flex-col flex-1 w-full col-span-1"
+                        >
+                          <div className="flex flex-row">
+                            <div
+                              onClick={() =>
+                                addSelected({
+                                  name: playlist.name,
+                                  id: playlist.id,
+                                  length: playlist.length,
+                                })
+                              }
+                              className={`
+                              relative w-14 h-14 before:absolute before:content-[''] before:h-full before:w-full before:bg-spotify-bg before:bottom-1 before:right-1 before:border-2 ${
+                                sel
+                                  ? "before:border-spotify-text before:bg-spotify-green"
+                                  : "before:border-spotify-text"
+                              }`}
+                            >
+                              <img
+                                src={playlist.image}
+                                className="relative block object-cover w-14 h-14 min-h-14 min-w-14 drop-shadow-lg"
+                              />
+                            </div>
+                            <div className="inline-flex flex-col items-start justify-end ml-2">
+                              <h3 className="block text-left text-[14px] font-normal tracking-tight first-letter:uppercase text-ellipsis whitespace-nowrap overflow-hidden">
+                                {playlist.name}
+                              </h3>
+                              <p className="relative flex flex-wrap z-10 font-medium text-[10px] uppercase text-neutral-700/80  whitespace-nowrap">
+                                Songs: #{playlist.length}
+                              </p>
                             </div>
                           </div>
-                        );
-                      }
-                    )}
+                        </div>
+                      );
+                    })}
+                    <button
+                      onClick={() => {
+                        getUserPlaylistTracks(selected, setPlaylistTunes);
+                      }}
+                      className="mt-10 flex items-center gap-4 px-4 py-1 text[14px] font-normal leading-relaxed tracking-tight rounded-lg 
+                      bg-spotify-green relative text-spotify-bg"
+                    >
+                      <IoPeopleSharp size={27} />
+                      Ready Up
+                    </button>
                   </div>
                 </div>
               </aside>
