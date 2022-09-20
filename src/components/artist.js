054e212faf259e21fdb7b9df36353fb467c35cd4
samsvk <samsvk00@gmail.com>
2022-09-20 15:21:53 +0100

    fixed

--- src/components/artist.js
+++ src/components/artist.js
@@ -18,7 +18,7 @@ export default function Artist() {
   return (
     <div className="relative w-full h-full">
       <img
-        src={randomArtist[0].image}
+        src={randomArtist[0].mainArtistInfo.image}
         className="absolute top-0 bottom-0 left-0 right-0 object-cover w-full h-full"
       />
     </div>
