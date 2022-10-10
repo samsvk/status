0cf4c31cf656641f7f7993a54984528b92843382
samsvk <samsvk00@gmail.com>
2022-10-10 23:11:52 +0100

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,8 @@
+import { BsArrowRight } from "react-icons/bs";
 export default function Navbar() {
   return (
     <div className="flex w-full border-b border-spotify-text/20">
-      <div className="relative w-full max-w-screen-sm">
+      <div className="relative flex items-center w-full pl-8">
         <div className="absolute flex items-center pr-8 bg-spotify-bg">
           <span className="relative block w-6 h-6 border-2 rounded-full z-8 bg-spotify-bg border-spotify-text" />
           <span className="relative z-0 block w-5 h-5 rounded-full right-3 bg-spotify-text border-spotify-text" />
@@ -13,32 +14,39 @@ export default function Navbar() {
       <nav className="flex ml-auto">
         <ul className="flex items-start justify-start w-full">
           <li
-            className="flex-1 px-8 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
             py-8 border-l border-r border-spotify-text/20 flex items-center justify-center
           "
           >
-            Company
+            How it works?
           </li>
           <li
-            className="flex-1 px-8 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
             py-8  border-r border-spotify-text/20 flex items-center justify-center
           "
           >
-            Company
+            Source Code
           </li>
           <li
-            className="flex-1 px-8 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
             py-8  border-r border-spotify-text/20 flex items-center justify-center
           "
           >
-            Company
+            Discord
           </li>
           <li
-            className="flex-1 px-8 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
             py-8  border-r border-spotify-text/20 flex items-center justify-center
           "
           >
-            Company
+            Contact Us
+          </li>
+          <li
+            className="flex-1 px-12  relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            py-8  border-r border-spotify-text/20 flex items-center justify-center min-w-[300px]
+          "
+          >
+            Donate & Support <BsArrowRight size={22} className="ml-2" />
           </li>
         </ul>
       </nav>
