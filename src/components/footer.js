e9b4377e852beea676c8424fcec6cf7074d7ed85
samsvk <samsvk00@gmail.com>
2022-10-11 19:12:15 +0100

    grid architecture

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,29 @@ import { GoFlame } from "react-icons/go";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 grid w-full h-2 grid-cols-7 bg-orange-500">
-      <div className="col-span-1 bg-pink-500"></div>
-      <div className="grid-cols-1 bg-indigo-500"></div>
-      <div className="col-span-2 bg-red-500"></div>
-      <div className="grid-cols-1 bg-sky-500"></div>
-      <div className="grid-cols-1 bg-blue-500"></div>
+    <div className="fixed bottom-0 grid items-end w-full grid-cols-7">
+      <div className="flex flex-col col-span-1 bg-pink-500 h-max">
+        <div className="bg-black min-h-[500px]"></div>
+      </div>
+      <div className="grid-cols-1 bg-indigo-500 h-max">
+        {" "}
+        <div className="bg-black min-h-[300px]"></div>
+      </div>
+      <div className="col-span-2 bg-red-500 h-max">
+        {" "}
+        <div className="bg-black min-h-[220px]"></div>
+      </div>
+      <div className="grid-cols-1 bg-sky-500 h-max">
+        {" "}
+        <div className="bg-black min-h-[180px]"></div>
+      </div>
+      <div className="grid-cols-1 bg-blue-500 h-max">
+        {" "}
+        <div className="bg-black min-h-[440px]"></div>
+      </div>
+      <div className="grid-cols-1 bg-stone-500 h-max">
+        <div className="bg-black min-h-[580px]"></div>
+      </div>
     </div>
   );
 }
