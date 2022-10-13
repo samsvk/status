18305d02061d68abb617d70b72b2bf4bdc26537b
samsvk <samsvk00@gmail.com>
2022-10-13 15:08:38 +0100

    navbar fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,52 +1,32 @@
 import { BsArrowRight } from "react-icons/bs";
+import { FaShareAlt } from "react-icons/fa";
+import { IoLink } from "react-icons/io5";
 export default function Navbar() {
   return (
-    <div className="flex w-full border-b-2 border-spotify-text">
-      <div className="relative flex items-center w-full pl-8">
+    <div className="fixed top-0 flex w-full py-10 pl-2">
+      <div className="relative flex items-center w-24 pl-8">
         <div className="absolute flex items-center pr-8 bg-spotify-bg">
           <span className="relative block w-6 h-6 border-2 rounded-full z-8 bg-spotify-bg border-spotify-text" />
           <span className="relative z-0 block w-5 h-5 rounded-full right-3 bg-spotify-text border-spotify-text" />
-          <h1 className="relative text-base font-medium tracking-normal z-8 text-spotify-text">
-            Shuff.le
-          </h1>
         </div>
       </div>
-      <nav className="flex ml-auto">
-        <ul className="flex items-start justify-start w-full">
+      <nav className="flex">
+        <ul className="flex items-center justify-center w-full gap-8">
           <li
-            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
-            py-8 border-l-2 border-r-2 border-spotify-text flex items-center justify-center
+            className="flex-1  relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            flex items-center justify-center gap-2 
           "
           >
-            How it works?
+            <IoLink size={28} />
+            Share
           </li>
           <li
-            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
-            py-8  border-r-2 border-spotify-text flex items-center justify-center
+            className="flex-1  relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
+            flex items-center justify-center gap-2
           "
           >
-            Source Code
-          </li>
-          <li
-            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
-            py-8  border-r-2 border-spotify-text flex items-center justify-center
-          "
-          >
-            Discord
-          </li>
-          <li
-            className="flex-1 px-12 relative text-base font-medium tracking-normal text-spotify-text whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
-            py-8  border-r-2 border-spotify-text flex items-center justify-center
-          "
-          >
-            Contact Us
-          </li>
-          <li
-            className="flex-1 px-12  relative text-base font-medium tracking-normal whitespace-nowrap after:absolute after:w-0 after:h-[2px] after:left-0 after:right-0 after:bottom-0 after:bg-spotify-text/5  hover:after:w-full after:duration-200 hover:cursor-pointer
-            py-8  border-r-2 border-spotify-text flex items-center justify-center min-w-[300px] text-spotify-green
-          "
-          >
-            Donate & Support <BsArrowRight size={22} className="ml-2" />
+            <FaShareAlt size={20} />
+            Share
           </li>
         </ul>
       </nav>
